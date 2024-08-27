827a8e290332e5f713309ee19e553908392cb210
Mike Macaulay <mike.macaulay@gmail.com>
2024-08-27 09:14:15 -0400

    Implement switching between regular season and crossover views

--- cfl-predictor/src/PageSeasonStandings.js
+++ cfl-predictor/src/PageSeasonStandings.js
@@ -1,9 +1,49 @@
-export function ContentSeasonStandings() {
+import {useState} from 'react';
+
+function ContentSeasonStandings() {
+  const [displayPage, setDisplayPage] = useState("regular-season");
   return(
       <div>
-          <p>
-          This is the content pane for Season Standings.
-          </p>
+          <SeasonStandingsNavBar setDisplayPage = {setDisplayPage} />
+          <SeasonTable displayPage={displayPage} />
       </div>
   );
 }
+
+function SeasonStandingsNavBar(props) {
+  return(
+    <div className="ButtonStrip">
+      <SeasonStandingsNavButton 
+        text='Regular Season Rankings'
+        setDisplayPage = {props.setDisplayPage}
+        pageName='pageA'
+      />
+      <SeasonStandingsNavButton
+        text='Crossover'
+        setDisplayPage = {props.setDisplayPage}
+        pageName='pageB'
+      />
+    </div> 
+  );  
+}
+
+function SeasonStandingsNavButton(props) {
+  function handleClick(){
+    props.setDisplayPage(props.pageName);
+  }
+  return(
+    <button onClick={handleClick}>
+      {props.text}
+    </button>
+  );
+}
+
+function SeasonTable(props) {
+  if(props.displayPage === 'pageA') return(<p>Page A</p>);
+  else if (props.displayPage === 'pageB') return(<p>Page B</p>);
+  else return(<p>Error</p>);
+}
+
+export {
+  ContentSeasonStandings
+};
